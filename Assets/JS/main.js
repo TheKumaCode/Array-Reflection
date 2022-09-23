2a304ae739eef36453f71d03bb496ae97b459b09
RJURU <110449011+RJURU@users.noreply.github.com>
2022-09-23 15:17:52 +0100

    New email gets selected in menu

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -8,10 +8,10 @@ const notifications = document.querySelector('.notifications')
 const closeSuccBtn = document.querySelector('.close')
 const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 const getImg = 'https://picsum.photos/300'
+const database = []
 let email
 let img
-let success = 0;
-const database = []
+let success = 0
 
 $(window).on('load', fetchImg)
 
@@ -102,7 +102,9 @@ function updateEmails() {
     var email = emailInput.value
     let newEmailElement = document.createElement('option')
     newEmailElement.textContent = email
+    newEmailElement.value = email
     eDropdown.appendChild(newEmailElement)
+    eDropdown.value = email
 }
 
 function updateDatabase() {
